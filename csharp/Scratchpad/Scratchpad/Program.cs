4f7cbaa713fc9933402a0c5dfae792a41d800412
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-26 16:44:29 -0700

    Render negative int as string

---
+++ csharp/Scratchpad/Scratchpad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Scratchpad
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Demo how negative int is rendered as string
+			int num = -123;
+			string str = num.ToString();
+			Console.WriteLine(str); // -123
+			char c = str[0];
+			Console.WriteLine(c); // -
+
+
+		}
+	}
+}
