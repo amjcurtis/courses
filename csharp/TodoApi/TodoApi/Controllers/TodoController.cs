5c6aeb33f4a02f1819183657bec2e75ff34ed37f
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-30 01:09:12 -0700

    Add 2 GET endpts and explicit controller routing

--- csharp/TodoApi/TodoApi/Controllers/TodoController.cs
+++ csharp/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -1,10 +1,13 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("api/todo")] // Routing to controller actions (e.g. 'todo' here) is case-insensitive in ASP.NET Core
 	[ApiController]
 	public class TodoController : Controller
 	{
@@ -19,6 +22,27 @@ namespace TodoApi.Controllers
 				_context.TodoItems.Add(new TodoItem { Name = "Item1" });
 				_context.SaveChanges();
 			}
+		}
+
+		// GET endpoint: api/Todo
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+		{
+			return await _context.TodoItems.ToListAsync();
+		}
+
+		// GET endpoint: api/Todo/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<TodoItem>> GetTodoItemById(long id)
+		{
+			TodoItem todoItem = await _context.TodoItems.FindAsync(id);
+
+			if (todoItem == null)
+			{
+				return NotFound(); // Returns 404 response
+			}
+
+			return todoItem;
 		}
 	}
 }
