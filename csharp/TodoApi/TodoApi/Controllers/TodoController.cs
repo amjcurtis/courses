0a5f88ec27efa199c39c281b4291b86ace0c846c
Andrew Curtis <curtis.andrewm@gmail.com>
2019-06-30 00:39:25 -0700

    Add ApiController decorator; remove unneeded comment

--- csharp/TodoApi/TodoApi/Controllers/TodoController.cs
+++ csharp/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Models;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TodoApi.Controllers
 {
 	[Route("api/[controller]")]
+	[ApiController]
 	public class TodoController : Controller
 	{
 		private readonly TodoListContext _context;
