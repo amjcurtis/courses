e473bae01dff63bb6b4a8e9299033a1c19b983b9
Andrew Curtis <curtis.andrewm@gmail.com>
2019-07-02 00:30:08 -0700

    Add working DELETE method

--- csharp/TodoApi/TodoApi/Controllers/TodoController.cs
+++ csharp/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -69,5 +69,22 @@ namespace TodoApi.Controllers
 
 			return NoContent();
 		}
+
+		// DELETE endpoint: api/Todo/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteTodoItem(long id)
+		{
+			TodoItem todoItem = await _context.TodoItems.FindAsync(id);
+
+			if (todoItem == null)
+			{
+				return NotFound();
+			}
+
+			_context.TodoItems.Remove(todoItem);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
 	}
 }
